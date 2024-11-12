f94b32b1f13b49b53dff2fe140f04cf64dd9700d
MarkusIngerslev <markusolsen2004@gmail.com>
2024-11-12 15:41:32 +0100

    Tilføjet collision mellem spiller og gamefield, samt tilføjet simpel readme for projekt

--- js/controller.js
+++ js/controller.js
@@ -4,13 +4,17 @@ import * as View from "./view.js";
 
 // Global variables
 const player = new Player(290, 210, 100);
-let lastTimestamp = 0;
 const controls = {
   left: false,
   right: false,
   up: false,
   down: false,
 };
+
+const GAMEFIELD_WIDTH = 640;
+const GAMEFIELD_HEIGHT = 480;
+
+let lastTimestamp = 0;
 
 // *************************
 //          Events
@@ -60,26 +64,44 @@ function handleKeyUp(event) {
 // *************************
 
 function movePlayer(deltaTime) {
+  // Start position for player
+  const position = { x: player.x, y: player.y };
   // Beregn distance baseret på deltaTime
   const distance = player.speed * (deltaTime / 1000);
 
-  // Tjek om en tast er trykket ned og flyt figuren
+  // Tjek om en tast er trykket ned og flyt positionen
   if (controls.up) {
-    player.move(0, -distance);
+    position.y -= distance;
   }
   if (controls.down) {
-    player.move(0, distance);
+    position.y += distance;
   }
   if (controls.left) {
-    player.move(-distance, 0);
+    position.x -= distance;
   }
   if (controls.right) {
-    player.move(distance, 0);
+    position.x += distance;
+  }
+
+  // Tjek om figuren kan flyttes til den nye position
+  if (canMove(player, position)) {
+    player.x = position.x;
+    player.y = position.y;
   }
 
   // Opdater visningen
   const { x, y } = player.getPosition();
   View.displayPlayer(x, y);
+}
+
+function canMove(player, position) {
+  // Tjek om den nye position er inden for grænserne af gamefield
+  return (
+    position.x >= 0 &&
+    position.x <= GAMEFIELD_WIDTH - 64 &&
+    position.y >= 0 &&
+    position.y <= GAMEFIELD_HEIGHT - 64
+  );
 }
 
 // *************************
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Mini Game
+
+Dette er et [lille spil](website) hvor det er muligt at flytte rundt på en karakter, samtidig med at der også er en **_fjende_** som man kan kollidere med.
+
+#### Udviklings trin
+
+- [Simpelt spil med fjende](#game-with-simple-enemy)
+
+
+### Game with simple enemy
+
+I første omgang af spillet, så når man `kollidere` med **_fjenden_**, som ikke styres af en `bruger`, så **_dør_** den karakter som `brugeren` kontrollere.
+
+Det kan ændres i senere implementeringer af mini spillet.
